5c54a45ffc7d3621cda45aaf725bc3d5dc726850
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-12-03 22:29:06 +0530

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredits from './pages/BuyCredits'
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <Result/>
+      <BuyCredits/>
+    </div>
+  )
+}
+
+export default App
