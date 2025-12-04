da53845a849bd609f4052cdf4cb506cc32c5d9e9
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-12-04 17:07:02 +0530

    updated Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-       <link to='/'><img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40'/></link>
+<img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40'/>
 
        <div>
 
