f7c705b93756e82c09297d086fb35ef4116a5bd0
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-12-04 11:47:23 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {assets} from '../assets/assets'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
-    const [user, setUser] = useState(null);
   return (
     <div>
        <link to='/'><img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40'/></link>
 
        <div>
-        {user}
 
        </div>
     </div>
